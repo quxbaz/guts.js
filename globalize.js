3a03a162ff010675c990a514560a95ff533f1c27
David Yeung <quxbaz@gmail.com>
2012-07-19 15:32:39 -0400

    Added globalize.js

---
+++ globalize.js
@@ -0,0 +1,24 @@
+/*
+  globalize.js
+
+  Injects objects into the global context.
+*/
+
+
+define(['underscore', 'util/util'], function(_, util) {
+
+    var that = this;
+    var globals = _.extend({}, util);
+
+    var globalize = function() {
+        /*
+          Pollutes the global namespace.
+        */
+        _.each(arguments, function(v, k) {
+            _.extend(globals, v);
+            _.extend(that, v);
+        });
+    };
+
+    return {globals: globals, globalize: globalize};
+});
